cee0c395f89110222901b442048b1a8fba895e17
ccccljs <cho38suk@hamail.net>
2021-10-24 07:55:59 +0900

    1이 될때까지

--- 1이 될때까지.py
+++ 1이 될때까지.py
@@ -1,19 +1,16 @@
-n=25
-k=5
-
+n,k = map(int,input().split)
 result = 0
 
-while n >= k: # n이 k보가 크거나 같을때
-
-    while n % k !=0: # 나누어 떨어지지 않을 때
-        n-=1 # 1을 뺀다
-        result+=1
-
-    n //= k # 나누기
+while n >= k: # n이 k이상이면
+    while n % k !=0: # n이 k로 나누어 떨어지지 않는다면
+        n -= 1 # 1 뻬기
+        result += 1
+    # 나누어 떨어진다면
+    n //= k # k로 나누기
     result += 1
 
-while n > 1: # 마지막으로 남은 수를 1만큼 뺀다
-    n-=1
-    result+=1
 
-print(result)
+while n > 1:
+    n -= 1
+    result += 1
+print(result);
