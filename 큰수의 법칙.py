e2c248e9f7c1c7a8408080b9923eaf20eede6f6e
ccccljs <cho38suk@hamail.net>
2021-10-24 08:38:09 +0900

    큰수의 법칙

--- 큰수의 법칙.py
+++ 큰수의 법칙.py
@@ -1,21 +1,10 @@
-n=5
-m=8
-k=3
+n = 1260
+count = 0
+coin_t = [500,100,50,10]
 
-data = [2, 4, 5, 4, 6]
-
-data.sort() # 데이터 정렬
-
-first=data[n-1] # 가장 큰수 = 6
-second=data[n-2] # 두번째로 큰수 = 5
-
-count = int(m/(k+1)) * k
-count += m % (k+1)
-
-result = 0
-result += (count) * first # 가장 큰수 더하기
-result += (m-count) * second # 두 번째로 큰수 더하기
-
-print(result)
+for coin in coin_t:
+    n %= coin
+    count+= n // coin
+print(count)
 
 
